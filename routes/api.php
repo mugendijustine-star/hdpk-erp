49591088b8fd2c02e906a020aa16599075d0487d
mugendijustine-star <mugendi.justine@gmail.com>
2025-12-02 10:18:20 +0300

    Add purchases controller and route

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Inventory\PurchasesController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum', 'trusted.device'])->group(function () {
+    Route::post('/purchases', [PurchasesController::class, 'store']);
+});
